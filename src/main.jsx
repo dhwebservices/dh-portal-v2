d69222f6555db517a664495843c9abbd8fd7738d
dhwebservices <admin@dhwebsiteservices.co.uk>
2026-04-29 19:38:29 +0100

    Load live redesign overrides

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/global.css'
+import './styles/redesign-overrides.css'
 import { applyPortalAppearance, readStoredPortalPreferences } from './utils/portalPreferences'
 
 // Set theme before render to prevent flash
